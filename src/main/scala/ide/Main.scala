94141c5095b292ff82c23bc5c0fef53217b0f51f
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2022-08-30 22:43:21 +0400

    .

--- src/main/scala/ide/Main.scala
+++ src/main/scala/ide/Main.scala
@@ -1,9 +1,7 @@
 package az.rock
 package ide
 
-import ide.Main.args
 import ide.run.concretes.RunnableApplication
-object Main extends App {
-  private val application = new RunnableApplication()
-  application.run()
-}
+
+@main
+def main() : Unit = new RunnableApplication().run()
