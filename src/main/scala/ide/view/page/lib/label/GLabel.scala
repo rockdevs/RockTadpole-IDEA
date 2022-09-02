7bddf25422cdab838bb51f82e777f73c73b36f0e
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2022-09-02 21:30:17 +0400

    .

--- src/main/scala/ide/view/page/lib/label/GLabel.scala
+++ src/main/scala/ide/view/page/lib/label/GLabel.scala
@@ -1,8 +1,11 @@
 package az.rock
 package ide.view.page.lib.label
 
+import java.awt.Font
 import javax.swing.JLabel
 
 class GLabel(text: String) extends JLabel(text:String) {
-
+  {
+    this.setFont(new Font("Helvetica",Font.PLAIN,16))
+  }
 }
