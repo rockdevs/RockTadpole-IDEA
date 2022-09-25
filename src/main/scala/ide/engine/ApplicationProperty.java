b7a404f1b1ac7d13806dfbbe420138188db68dc5
Vugar Mammadli <59668725+l00pss@users.noreply.github.com>
2022-09-25 13:35:47 +0400

    ApplicationExecutor updated

--- src/main/scala/ide/engine/ApplicationProperty.java
+++ src/main/scala/ide/engine/ApplicationProperty.java
@@ -1,6 +1,6 @@
 package az.rock.ide.engine;
 
 public class ApplicationProperty {
-    public static String NAME = "RockTadpole-IDEA";
+    public static String NAME = "RockTadpole IDEA";
     public static String VERSION = "0.1.0-SNAPSHOT";
 }
--- src/main/scala/ide/view/page/screen/intro/IntroSidePanel.java
+++ src/main/scala/ide/view/page/screen/intro/IntroSidePanel.java
@@ -2,19 +2,26 @@ package az.rock.ide.view.page.screen.intro;
 
 import az.rock.ide.engine.ApplicationProperty;
 import az.rock.ide.view.ui.frame.GPrimaryProcessFrame;
+import az.rock.ide.view.ui.icon.enums.GIconBar;
 import az.rock.ide.view.ui.panel.GSimplePanel;
+import az.rock.ide.view.util.ImageProvider;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class IntroSidePanel extends GSimplePanel {
     private final GPrimaryProcessFrame primaryProcessFrame;
 
 
-    private GSimplePanel iconPanel = new GSimplePanel();
+    private GSimplePanel iconPanel = new IconPanel();
 
 
 
 
     public IntroSidePanel(GPrimaryProcessFrame processFrame){
         this.primaryProcessFrame = processFrame;
+        this.setLayout(new BorderLayout());
+        this.add(this.iconPanel,BorderLayout.NORTH);
     }
     
     
@@ -24,19 +31,42 @@ public class IntroSidePanel extends GSimplePanel {
 
     @Override
     public void componentInitializer() {
-        
+        iconPanel.componentInitializer();
     }
 
 
 
     class IconPanel extends GSimplePanel{
-        private final String header = ApplicationProperty.NAME;
-        private final String version = ApplicationProperty.VERSION;
+        private final JLabel image = new JLabel();
+        {
+            try {
+                this.image.setIcon(new ImageIcon(ImageProvider.getImage(GIconBar.MAIN_ICON.path(),new Dimension(50,50))));
+            }catch (Exception exception){
 
-        public IconPanel(){
-            
+            }
         }
-        
+        private final JLabel header = new JLabel(ApplicationProperty.NAME);
+        private final JLabel version = new JLabel(ApplicationProperty.VERSION);
+        private GSimplePanel textPanel = new GSimplePanel();
+        {
+            //JLabel Configuration
+            this.header.setFont(new Font("Roboto",Font.PLAIN,18));
+            this.version.setForeground(Color.gray);
+        }
+
+        {
+            //TextPanel COnfiguration
+            this.textPanel.setLayout(new BorderLayout());
+            this.textPanel.add(this.header,BorderLayout.NORTH);
+            this.textPanel.add(this.version,BorderLayout.SOUTH);
+        }
+        public IconPanel(){
+            this.setLayout(new FlowLayout(FlowLayout.LEFT));
+            this.add(this.image);
+            this.add(this.textPanel);
+            this.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+        }
+
         @Override
         public void componentInitializer() {
 
